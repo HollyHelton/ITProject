a10a073b1f8f0e5f77ed43177792f4ebc9c64322
hollyhelton <hollyrachelhelton@gmail.com>
2016-11-23 13:31:39 +0000

    Started DAL

---
+++ DroneSite/DAL/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneSite.DAL
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+    }
+}
---
+++ DroneSite/Users/RoleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneSite.Users
+{
+    internal class RoleManager
+    {
+        //Creates new user and role, if role doesn't already exist
+        internal void AddUserAndRole()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            IdentityResult IdRoleResult;
+            IdentityResult IdUserResult;
+        }
+    }
+}
